7ee88614f14bd6c913cf5f47eb4ed09bdb020c1e
stu-cjlu-sp <152497050+stu-cjlu-sp@users.noreply.github.com>
2024-07-25 15:09:11 +0800

    Delete MTL-Dataset directory

--- MTL-Dataset/dataset/MSST_Y.m
+++
@@ -1,7 +0,0 @@
-function  TFD=MSST_Y(y13)
-y13=real(y13);
-Ts=MSST_Y_new(y13',64,3);
-TFD = abs(Ts);
-TFD = imresize(TFD,[256 256]);
-end
-
--- MTL-Dataset/dataset/T3.m
+++
@@ -1,6 +0,0 @@
-function   y=T3(fc,t,n,F,T,SNR)
-pha=[];
-pha=mod(2*pi/n*ceil(n*F*t.^2/(2*T)),2*pi);
-y=exp(1i*(2*pi*fc*t+pha));
-% y=awgn(y,SNR);
-end
